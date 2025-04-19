a559ade87bdba569351fb171facc86bbb3afe7e0
kaoru0705 <kaoru9875@gmail.com>
2025-04-19 21:52:33 +0900

    string class
    JAVA처럼 String은 아니네
    "std::string"이라고 적어야 한다.

---
+++ DoitCPP/ch02/std_string/std_string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include <string>	// string in iostream
+
+int main() {
+	std::string string_value("Hello~");
+	std::cout << string_value << std::endl;
+
+	string_value = "HAHA";
+	std::cout << string_value << std::endl;
+
+	return 0;
+}
