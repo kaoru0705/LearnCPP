bd043c69d3f95b95b42506a7ce9c0e4c19c0d29d
kaoru0705 <kaoru9875@gmail.com>
2025-04-19 21:30:52 +0900

    UNICODE FLOAT

---
+++ DoitCPP/ch02/UNICODE/UNICODE.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+
+
+int main() {
+	wchar_t message_korean[] = L"반갑다 세계야!";      // 한국어 
+	wchar_t message_chinese[] = L"你好，世界!";        // 중국어 
+	wchar_t message_japanese[] = L"ハロー・ワール!";   // 일본어 
+	wchar_t message_russian[] = L"Привет мир!";        // 러시아어
+
+	std::cout << "Hello, world!" << std::endl;
+
+	_setmode(_fileno(stdout), _O_U16TEXT); // 윈도우 콘솔 창 유니코드 출력 모드 설정
+
+	std::wcout << message_korean << std::endl;
+	std::wcout << message_chinese << std::endl;
+	std::wcout << message_japanese << std::endl;
+	std::wcout << message_russian << std::endl;
+
+	return 0;
+}
---
+++ DoitCPP/ch02/precision_floating_point/precision_floating_point.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+/* https://en.cppreference.com/w/cpp/types/numeric_limits/digits10 */
+// std::setprecision(std::numeric_limits<double>::digits10 + 1) 이건 진수 기반으로 오버플로우나 라운딩 없이 유효숫자를 조정할 수 있다. +1을 하면 한 자리 더
+int main() {
+    float float_value = 9.87654321f;
+    double double_value = 9.87654321987654321;
+    long double long_double_value = 9.87654321987654321l;
+
+    std::cout << "float : " << sizeof(float) << " bytes" << std::endl;
+    std::cout << "float_value : " <<
+        std::setprecision(std::numeric_limits<float>::digits10 + 1) <<
+        float_value << std::endl << std::endl;
+
+    std::cout << "double : " << sizeof(double) << " bytes" << std::endl;
+    std::cout << "double_value : " << 
+        std::setprecision(std::numeric_limits<double>::digits10 + 1) <<
+        double_value << std::endl << std::endl;
+
+    std::cout << "long double : " << sizeof(long double) << " bytes" << std::endl;
+    std::cout << "long_double_value : " <<
+        std::setprecision(std::numeric_limits<long double>::digits10 + 1) <<
+        long_double_value << std::endl << std::endl;
+
+    return 0;
+}
