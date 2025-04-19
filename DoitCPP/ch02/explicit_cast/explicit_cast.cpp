ea0d77c7f0fb91f293ab2bef76246504b7a02104
kaoru0705 <kaoru9875@gmail.com>
2025-04-19 22:57:21 +0900

    c++ style
    not (float) float()

---
+++ DoitCPP/ch02/explicit_cast/explicit_cast.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+
+int main() {
+	int int_a = 10;
+	int int_b = 5;
+
+	int int_avg = (int_a + int_b) / 2;
+	float float_avg_1 = (int_a + int_b) / 2;        // 암시적 형변환: 데이터 유실
+	float float_avg_2 = float(int_a + int_b) / 2;   // 명시적 형변환 c++ style
+
+	std::cout << "int_avg: " << int_avg << std::endl;
+	std::cout << "float_avg_1: " << float_avg_1 << std::endl;
+	std::cout << "float_avg_2: " << float_avg_2 << std::endl;
+
+	return 0;
+}
