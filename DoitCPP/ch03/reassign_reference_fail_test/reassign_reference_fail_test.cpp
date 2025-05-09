a8e49c1bfb1a41250844d7f3c8ca2c46ef00b030
kaoru0705 <kaoru9875@gmail.com>
2025-05-09 16:25:11 +0900

    reference reassign fail
    포인터와는 다르게 레퍼런스 변수는 재지정을 할 수 없다. 메모리 크기, 주소를 변경하는 것 없이 안정적으로 원본 값의 접근할 수 있다.

---
+++ DoitCPP/ch03/reassign_reference_fail_test/reassign_reference_fail_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+	int value = 10;
+	int& ref_value = value;
+
+	int value2 = 20;
+	ref_value = value2;   // 레퍼런스 재지정 불가 그냥 값을 바꿨다. 따라서 value의 값도 20이 된다.
+
+	std::cout << "value: " << value << std::endl;
+	std::cout << "ref_value: " << ref_value << std::endl;
+
+	return 0;
+}
