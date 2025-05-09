ae53b3f22733ba3d70c5e1b61bd1ac8e1407ecb7
kaoru0705 <kaoru9875@gmail.com>
2025-05-09 14:18:06 +0900

    Update precision_floating_point.cpp
    +1이 최대 유효자릿수

--- DoitCPP/ch02/precision_floating_point/precision_floating_point.cpp
+++ DoitCPP/ch02/precision_floating_point/precision_floating_point.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <iomanip>
 /* https://en.cppreference.com/w/cpp/types/numeric_limits/digits10 */
-// std::setprecision(std::numeric_limits<double>::digits10 + 1) 이건 진수 기반으로 오버플로우나 라운딩 없이 유효숫자를 조정할 수 있다. +1을 하면 한 자리 더
+// std::setprecision(std::numeric_limits<double>::digits10 + 1) 이건 진수 기반으로 오버플로우나 라운딩 없이 유효숫자를 조정할 수 있다. +1이 최대 유효자릿수이다.
 int main() {
     float float_value = 9.87654321f;
     double double_value = 9.87654321987654321;
@@ -11,6 +11,10 @@ int main() {
     std::cout << "float_value : " <<
         std::setprecision(std::numeric_limits<float>::digits10 + 1) <<
         float_value << std::endl << std::endl;
+
+    std::cout << "float : " << sizeof(float) << " bytes" << std::endl;
+    std::cout << "float_value : "  << float_value << std::endl << std::endl;
+
 
     std::cout << "double : " << sizeof(double) << " bytes" << std::endl;
     std::cout << "double_value : " << 
