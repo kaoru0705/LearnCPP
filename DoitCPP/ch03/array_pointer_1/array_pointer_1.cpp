527221cbe7d224896dc3b85c7123a800defb1e4a
kaoru0705 <kaoru9875@gmail.com>
2025-05-09 15:03:07 +0900

    new delete
    calloc malloc free 뿐만 아니라 java처럼 동적할당으로 new가 있다. 다만 delete로 없애줘야 한다.

---
+++ DoitCPP/ch03/array_pointer_1/array_pointer_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int main() {
+    int lotto[45] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                      16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                      31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
+
+    std::cout << "lotto[0] Address: " << &lotto[0] << std::endl;
+    std::cout << "lotto[1] Address: " << &lotto[1] << std::endl;
+    std::cout << "lotto[2] Address: " << &lotto[2] << std::endl;
+    std::cout << "lotto[3] Address: " << &lotto[3] << std::endl;
+    std::cout << "lotto[4] Address: " << &lotto[4] << std::endl;
+    std::cout << "lotto[5] Address: " << &lotto[5] << std::endl << std::endl;
+    
+    std::cout << "(lotto + 0) Address: " << lotto + 0 << std::endl;
+    std::cout << "(lotto + 1) Address: " << lotto + 1 << std::endl;
+    std::cout << "(lotto + 2) Address: " << lotto + 2 << std::endl;
+    std::cout << "(lotto + 3) Address: " << lotto + 3 << std::endl;
+    std::cout << "(lotto + 4) Address: " << lotto + 4 << std::endl;
+    std::cout << "(lotto + 5) Address: " << lotto + 5 << std::endl;
+
+
+    return 0;
+}
---
+++ DoitCPP/ch03/new_and_delete/new_and_delete_array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main() {
+    int* pt_int_array_value = new int[5];   // 동적 메모리 할당 (배열)
+
+    for (int i = 0; i < 5; i++) {
+        pt_int_array_value[i] = i;   // 할당된 배열 변수에 0~4까지 순서대로 넣기
+    }
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << pt_int_array_value[i] << std::endl;   // 배열 변수 출력
+    }
+
+    delete[] pt_int_array_value;   // 동적 메모리 해제(배열)
+
+    return 0;
+}
