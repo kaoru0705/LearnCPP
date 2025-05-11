b8d387471198ced0e00c749351419a411249b05b
kaoru0705 <kaoru9875@gmail.com>
2025-05-11 15:17:17 +0900

    assert
    true가 아니면 프로그램 실행이 중단된다.

---
+++ DoitCPP/ch05/assert1/assert1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cassert>  
+
+
+void print_number(int* _pt_int) {
+	assert(_pt_int != NULL);
+	std::cout << *_pt_int << std::endl;
+}
+
+int main() {
+	int a = 100;
+	int* b = NULL;
+	int* c = NULL;
+
+	b = &a;
+	print_number(b);
+
+	// c는 NULL인 상태로 인자 전달
+	print_number(c);
+
+	return 0;
+}
