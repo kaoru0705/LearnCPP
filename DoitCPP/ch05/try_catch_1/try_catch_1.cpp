63dfa35af3d5a36a87282020dab8d4160cd94f81
kaoru0705 <kaoru9875@gmail.com>
2025-05-11 15:45:26 +0900

    s

---
+++ DoitCPP/ch05/try_catch_1/try_catch_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    try
+    {
+        // Statements that may throw exceptions you want to handle go here
+        throw std::string("dsdsa"); // here's a trivial example
+    }
+    catch (double) // no variable name since we don't use the exception itself in the catch block below
+    {
+        // Any exceptions of type double thrown within the above try block get sent here
+        std::cerr << "We caught an exception of type double\n";
+    }
+    catch (int x)
+    {
+        // Any exceptions of type int thrown within the above try block get sent here
+        std::cerr << "We caught an int exception with value: " << x << '\n';
+    }
+    catch (const std::string&) // catch classes by const reference
+    {
+        // Any exceptions of type std::string thrown within the above try block get sent here
+        std::cerr << "We caught an exception of type std::string\n";
+    }
+
+    // Execution continues here after the exception has been handled by any of the above catch blocks
+    std::cout << "Continuing on our merry way\n";
+
+    return 0;
+}
