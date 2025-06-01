a09ad19b3680fd660dcd563c601740046da89eb7
kaoru0705 <kaoru9875@gmail.com>
2025-06-01 17:04:03 +0900

    dynamic-list-initialization

--- DoitCPP/ch03/new_and_delete/new_and_delete_array.cpp
+++ DoitCPP/ch03/new_and_delete/new_and_delete_array.cpp
@@ -2,7 +2,7 @@
 
 
 int main() {
-    int* pt_int_array_value = new int[5];   // 동적 메모리 할당 (배열)
+    int* pt_int_array_value{ new int[5] {1, 2, 3, 4, 5} };   // 동적 메모리 할당 (배열)
 
     for (int i = 0; i < 5; i++) {
         pt_int_array_value[i] = i;   // 할당된 배열 변수에 0~4까지 순서대로 넣기
