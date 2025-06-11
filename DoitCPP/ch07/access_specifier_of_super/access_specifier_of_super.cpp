578bf17bd2315a2901c9785ca6cf6b9765dc065c
kaoru0705 <kaoru9875@gmail.com>
2025-06-11 15:43:33 +0900

    Update access_specifier_of_super.cpp

--- DoitCPP/ch07/access_specifier_of_super/access_specifier_of_super.cpp
+++ DoitCPP/ch07/access_specifier_of_super/access_specifier_of_super.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-
+/*
+Cpp는 클래스의 접근지정자를 지정하는 게 아니라 상속받을 클래스의 접근지정자를 지정할 수 있다.
+public으로 지정할 경우 그대로 이며 private이면 전부 private으로 바뀌고, protected이면 public변수는 protected로 바뀐다.
+*/
 class Base {
 public:
 	int a;
