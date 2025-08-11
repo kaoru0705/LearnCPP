8343174fa9656ba00626409236fffc8fce5567ac
kaoru0705 <kaoru9875@gmail.com>
2025-08-11 16:25:04 +0900

    constructor
    상속받는 부모클래스의 순서, 그다음에 자식클래스의 생성자가 실행된다.

---
+++ DoitCPP/ch07/inheritance_constructor/inheritance_constructor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <random>
+
+
+class character {
+public:
+    character() {
+        std::cout << "character 클래스 생성자" << std::endl;
+    };
+};
+
+class monster {
+public:
+    monster() {
+        std::cout << "monster 클래스 생성자" << std::endl;
+    };
+};
+
+//몬스터 A는 기본 Monster 클래스로부터 상속
+class monster_a : public monster, character {
+public:
+    monster_a() {
+        std::cout << "monster_a 클래스 생성자" << std::endl;
+    };
+};
+
+int main() {
+    monster_a forest_monster;
+    return 0;
+}
