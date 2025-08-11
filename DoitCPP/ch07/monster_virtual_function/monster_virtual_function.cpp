c1bcf42a1e3aa6878811fc60ee2cb9e994a510c0
kaoru0705 <kaoru9875@gmail.com>
2025-08-11 16:17:51 +0900

    virtual function
    virtual을 멤버함수에 붙이면 부모클래스를 참조하더라도 객체에서 가장 마지막에 오버라이딩된 함수를 호출한다.

---
+++ DoitCPP/ch07/monster_virtual_function/monster_virtual_function.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+class character {
+public:
+	character() : hp(100), power(100)
+	{
+	};
+	void get_damage(int _damage) {};
+
+protected:
+	int hp;
+	int power;
+};
+
+//Player 클래스
+class player : public character {
+public:
+	player() {};
+};
+
+//기본 Monster 클래스
+class monster {
+public:
+	monster() {};
+	void attack(player target_player) {};
+	virtual void attack_special(player target_player);
+};
+
+void monster::attack_special(player target_player) {
+	std::cout << "기본 공격 : 데미지 - 10 hp" << std::endl;
+}
+
+//몬스터 A는 기본 Monster 클래스로부터 상속
+class monster_a : public monster, character {
+public:
+	//상속받은 함수 오버라이딩 
+	virtual void attack_special(player target_player) override;
+};
+
+void monster_a::attack_special(player target_player) {
+	std::cout << "인텡글 공격 : 데미지 - 15 hp" << std::endl;
+}
+
+//몬스터 B는 기본 Monster 클래스로부터 상속
+class monster_b : public monster, character {
+public:
+	//상속받은 함수 오버라이딩
+	virtual void attack_special(player target_player) override;
+};
+
+void monster_b::attack_special(player target_player) {
+	std::cout << "가상 공격 : 데미지 - 0 hp" << std::endl;
+}
+
+//몬스터 C는 기본 Monster 클래스로부터 상속
+class monster_c : public monster, character {
+public:
+	//상속받은 함수 오버라이딩
+	virtual void attack_special(player target_player) override;
+};
+
+void monster_c::attack_special(player target_player) {
+	std::cout << "강력 뇌전 공격 : 데미지 - 100 hp" << std::endl;
+}
+
+int main() {
+	player player_1;
+
+	monster_a forest_monster;
+
+	monster& mon = forest_monster;
+	monster_a& mon_a = forest_monster;
+
+	std::cout << std::endl << "부모 클래스 레퍼런스 후 공격" << std::endl;
+	mon.attack_special(player_1);
+
+	std::cout << std::endl << "자식 클래스 레퍼런스 후 공격" << std::endl;
+	mon_a.attack_special(player_1);
+
+	std::cout << std::endl << "범위 연산자 활용 공격" << std::endl;
+	mon_a.monster::attack_special(player_1);
+
+	return 0;
+}
