0868773f4112c995a413044e05ef03b9035e3927
kaoru0705 <kaoru9875@gmail.com>
2025-08-11 23:24:54 +0900

    no change

--- DoitCPP/ch07/member_initialization_order/member_inialization_order.cpp
+++ DoitCPP/ch07/member_initialization_order/member_inialization_order.cpp
@@ -1,19 +1,18 @@
 #include <algorithm> // for std::max
 #include <iostream>
+/* https://www.learncpp.com/cpp-tutorial/constructor-member-initializer-lists/ */
+//Because the C++ standard says so, the members in a member initializer list are always initialized in the order in which they are defined inside the class
+//(not in the order they are defined in the member initializer list).
 
-/*
-Because the C++ standard says so, the members in a member initializer list are always initialized in the order in which they are defined inside the class
-(not in the order they are defined in the member initializer list).
-*/
 class Foo
 {
 private:
-    int m_x{};
+    int m_x{};  // default member initializer
     int m_y{};
 
 public:
     Foo(int x, int y)
-        : m_y{ std::max(x, y) }, m_x{ m_y } // issue on this line
+        : m_y{ std::max(x, y) }, m_x{ m_y } // issue on this line (member initializer list)
     {
     }
 
