57adc5ce9784bb30d108c98ffdfbbe220efb1f42
kaoru0705 <kaoru9875@gmail.com>
2025-08-18 13:54:54 +0900

    virtual ~클래스이름

---
+++ DoitCPP/ch08/virtual_destructor_second/virtual_destructor_second.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+//기본 Monster 클래스
+class monster {
+public:
+	monster();
+	//~monster();
+	virtual ~monster();
+private:
+	int* dummy;
+};
+
+monster::monster() {
+	std::cout << "monster() 생성자 호출" << std::endl;
+	dummy = new int;
+}
+
+monster::~monster() {
+	std::cout << "~monster() 소멸자 호출" << std::endl;
+	delete dummy;
+}
+
+//몬스터 A는 기본 Monster 클래스로부터 상속
+class monster_a : public monster {
+public:
+	monster_a();
+	//~monster_a();
+	virtual ~monster_a();
+private:
+	int* dummy_a;
+};
+
+monster_a::monster_a() {
+	std::cout << "monster_a() 생성자 호출" << std::endl;
+	dummy_a = new int;
+}
+
+monster_a::~monster_a() {
+	std::cout << "~monster_a() 소멸자 호출" << std::endl;
+	delete dummy_a;
+}
+
+int main() {
+	monster* mon = new monster_a();   // 부모 클래스로 업캐스팅
+	delete mon;
+	return 0;
+}
