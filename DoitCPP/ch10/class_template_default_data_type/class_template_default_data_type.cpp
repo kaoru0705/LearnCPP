4c6c36608e879d4f0bc8fe527d7f032ecc667bcc
kaoru0705 <kaoru9875@gmail.com>
2025-08-20 15:00:10 +0900

    default data type
    클래스 매개변수의 기본형을 제시하면 객체를 생성할 때 데이터 형식을 생략할 수 있다.

---
+++ DoitCPP/ch10/class_template_default_data_type/class_template_default_data_type.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+template <typename T = int>   // 기본 형식 설정
+class data_package {
+public:
+    data_package(T first) : first{ first } {}
+    void print_out_element() {
+        std::cout << "클래스 템플릿 형식 매개 변수 값 : " << first << std::endl;
+    }
+private:
+    T first;
+};
+
+int main() {
+    data_package<> template_inst1{ 5 };   // 기본 형식(여기서는 int)으로 지정
+    data_package<std::string> template_inst2{"클래스 템플릿 기본값"};
+
+    template_inst1.print_out_element();
+    template_inst2.print_out_element();
+}
