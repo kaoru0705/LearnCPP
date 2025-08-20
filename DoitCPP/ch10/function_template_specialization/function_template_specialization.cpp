0e2e50bf13047b0b44161a5c6234588d88ceff88
kaoru0705 <kaoru9875@gmail.com>
2025-08-20 11:43:47 +0900

    func temp specialization
    템플릿 함수를 특수화시켜서 특정 데이터 형식만 다르게 처리할 수 있다.

--- DoitCPP/ch10/function_template_specialization/function_template_specialization.cpp
+++ DoitCPP/ch10/function_template_specialization/function_template_specialization.cpp
@@ -6,19 +6,19 @@ T data_sum(T operand1, T operand2) {
 	return operand1 + operand2;
 }
 
+template <>
+double data_sum(double operand1, double operand2) {
+	return  (int)operand1 + (int)operand2;
+}
+
 int main() {
-	int data1{ 3 }, data2{ 5 };
-	double data3{ 4.5 }, data4{ 8.9 };
-	std::string data5{ "Hello, " }, data6{ "World!" };
-	char data7[]{ "Hello, " }, data8[]{ "New World!" };
+	int data1 = 3, data2 = 5;
+	double data3 = 4.5, data4 = 8.9;
+	std::string data5 = "Hello, ", data6 = "World!";
 
 	std::cout << "정수형 데이터 합: " << data_sum(data1, data2) << std::endl;
 	std::cout << "실수형 데이터 합: " << data_sum(data3, data4) << std::endl;
 	std::cout << "문자열 데이터 합: " << data_sum(data5, data6) << std::endl;
-
-	// 사용할 수 없는 인자 예제
-	//std::cout << "문자데이터 배열의 합" << data_sum(data7, data8) << std::endl;
-	std::cout << "문자데이터 배열의 합: " << data_sum<std::string>(data7, data8) << std::endl;
 
 	return 0;
 }
