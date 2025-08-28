79f03a6ba2857078dd970745f23216b585e9238e
kaoru0705 <kaoru9875@gmail.com>
2025-08-28 11:12:24 +0900

    std::tie
    tuple을 해체해서 각각의 원소를 변수에 매칭시킨다. std::ignore로 무시가능

---
+++ DoitCPP/ch15/tuple_decomposition/tuple_decomposition.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <tuple>
+
+const int monster_a_type{ 0 };
+const int monster_b_type{ 1 };
+const int monster_c_type{ 2 };
+
+class monster {
+public:
+    int get_monster_type() { return monster_type; };
+
+protected:
+    int monster_type;
+
+private:
+    int location_x;
+    int location_y;
+};
+
+//몬스터 A는 추상 클래스 Monster 클래스로부터 상속
+class monster_a : public monster {
+public:
+    monster_a() {
+        monster_type = monster_a_type;
+    };
+};
+
+
+//몬스터 B는 추상 클래스 Monster 클래스로부터 상속
+class monster_b : public monster {
+public:
+    monster_b() {
+        monster_type = monster_b_type;
+    };
+};
+
+
+//몬스터 C는 추상 클래스 Monster 클래스로부터 상속
+class monster_c : public monster {
+public:
+    monster_c() {
+        monster_type = monster_c_type;
+    };
+};
+
+std::tuple<int, std::string, int, int> get_monster_status(monster& monster_inst) {
+
+    int monster_type = monster_inst.get_monster_type();
+    std::string monster_name;
+    int hp{ 100 }, power{ 100 };
+
+    switch (monster_type) {
+    case monster_a_type:
+        monster_name = "A 몬스터";
+        hp += 10;
+        break;
+    case monster_b_type:
+        monster_name = "B 몬스터";
+        power += 20;
+        break;
+    case monster_c_type:
+        monster_name = "C 몬스터";
+        hp -= 10;
+        power += 100;
+        break;
+    }
+
+    return make_tuple(monster_type, monster_name, hp, power);
+}
+
+void print_out_tuple(std::tuple<int, std::string, int, int> monster_status) {
+    std::cout << std::get<1>(monster_status) << "("
+        << std::get<0>(monster_status) << ") : hp("
+        << std::get<2>(monster_status) << "), power("
+        << std::get<3>(monster_status) << ")" << std::endl;
+}
+
+int main() {
+    int monster_type, hp, power;
+    monster_a monster_a_inst;
+
+    std::tuple<int, std::string, int, int> monster_a_status{ get_monster_status(monster_a_inst) };
+    // 튜플의 개별 변수에 매칭
+    std::tie(monster_type, std::ignore, hp, power) = get_monster_status(monster_a_inst);
+
+    print_out_tuple(monster_a_status);
+    std::cout << "tie()로 변수에 매핑" << std::endl << "monster type(" << monster_type << ") : hp(" << hp << "), power(" << power << ")" << std::endl;
+
+    return 0;
+}
